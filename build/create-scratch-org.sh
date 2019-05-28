a0d8171fbb5f822e6ec6feabc17ad7e57f1e6a3a
venkatesh457 <malasanivenkatesh045@gmail.com>
2019-05-29 00:18:32 +0530

    sample1

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -5,7 +5,7 @@ echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_QAT_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_QAT_USER --setdefaultdevhubusername -a DevHub
+sfdx force:auth:jwt:grant --clientid $SFDC_QAT_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_QAT_USER --setdefaultdevhubusername -a Qat
 
 #Create a scratch org
 echo "Creating the Scratch Org..."
