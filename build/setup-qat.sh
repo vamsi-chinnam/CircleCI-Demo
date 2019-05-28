9601d14e093f0aa9792b233f2d0609f7a67e3333
venkatesh457 <malasanivenkatesh045@gmail.com>
2019-05-29 00:39:08 +0530

    changes in scratch3

--- build/setup-qat.sh
+++ build/setup-qat.sh
@@ -4,4 +4,4 @@ mkdir keys
 echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_UAT_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_UAT_USER --setdefaultdevhubusername -a UAT
+sfdx force:auth:jwt:grant --clientid $SFDC_QAT_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_QAT_USER --setdefaultdevhubusername -a Qat
