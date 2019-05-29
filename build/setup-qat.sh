f3e40522700a976a597e906d9405f00b30079f17
venkatesh457 <malasanivenkatesh045@gmail.com>
2019-05-29 11:46:00 +0530

    keep only prod config

--- build/setup-qat.sh
+++ build/setup-qat.sh
@@ -1,8 +1,7 @@
 # Get the private key from the environment variable
-echo "Setting up QAT Connection..."
+echo "Setting up UAT Connection..."
 mkdir keys
 echo $SFDC_SERVER_KEY | base64 -d > keys/server.key
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDC_QAT_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_QAT_USER --setdefaultdevhubusername -a QAT
-echo "QAT Authenticated..."
+sfdx force:auth:jwt:grant --clientid $SFDC_QAT_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_QAT_USER --setdefaultdevhubusername -a Qat
